149e2b2504addc430f5552d73ac1026a056d8fe7
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-23 02:34:27 +0100

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,12 @@
 
+resource "google_compute_network" "vpc" {
+  name                    = "sql-with-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet-with-sql"
+  ip_cidr_range = "10.1.0.0/21"
+  region        = "us-central1"
+  network       = google_compute_network.vpc.name
+}
