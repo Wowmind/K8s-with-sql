72e0af3a9d2569ddd49cce106dc7f5b8f0702199
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-23 22:41:09 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_container_cluster" "private"{
 name           = "k8s-cluster"
 location       = "us-central1"
-
+network        = google_compute_network.vpc.name
+subnetwork     = google_compute_subnetwork.subnet.name
 }
