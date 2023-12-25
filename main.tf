76c3639b4af3fb5948b90801b838ff9d7955890c
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-25 15:12:21 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -35,11 +35,11 @@ resource "google_compute_global_address" "private-ip-peering" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 24
-  network       = google_compute_network.custom.id
+  network       = google_compute_network.vpc.id
 }
 
 resource "google_service_networking_connection" "private-vpc-connection" {
-  network = google_compute_network.custom.id
+  network = google_compute_network.vpc.id
   service = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [
     google_compute_global_address.private-ip-peering.name
