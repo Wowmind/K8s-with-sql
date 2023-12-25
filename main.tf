fd485e979a3bee9d420fd7f14d301a7c44ca27ca
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-25 14:27:27 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -3,4 +3,11 @@ name           = "k8s-cluster"
 location       = "us-central1"
 network        = google_compute_network.vpc.name
 subnetwork     = google_compute_subnetwork.subnet.name
+
+private_cluster_config {
+    enable_private_endpoint = false
+    enable_private_nodes    = true
+    master_ipv4_cidr_block  = var.gke_master_ipv4_cidr_block
+  }
+
 }
