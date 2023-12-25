376a0a27e1cdc68510cb6d4eb3648e97b905300a
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-25 15:10:23 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,21 @@ ip_allocation_policy {
   release_channel {
     channel = "REGULAR"
   }
+}
 
+# Create an sql instance
+resource "google_compute_global_address" "private-ip-peering" {
+  name          = "google-managed-services-custom"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = google_compute_network.custom.id
+}
 
+resource "google_service_networking_connection" "private-vpc-connection" {
+  network = google_compute_network.custom.id
+  service = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [
+    google_compute_global_address.private-ip-peering.name
+  ]
 }
