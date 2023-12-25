6c908d65e0e421031ad7d5cf379d55d615730c94
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-25 14:38:14 +0100

    variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,16 @@
 
+variable "project_id" {}
+
+variable "credentials" {}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-b"
+}
+
 variable "gke_master_ipv4_cidr_block" {
 default        = "10.2.0.0/21
   }
