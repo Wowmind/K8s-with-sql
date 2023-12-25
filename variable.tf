52a0ccc1fd7520688272e4a2ce0678c4d00d54bf
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-25 15:05:00 +0100

    variable.tf

--- variable.tf
+++ variable.tf
@@ -4,15 +4,41 @@ variable "project_id" {}
 variable "credentials" {}
 
 variable "region" {
+  type    = string
   default = "us-central1"
 }
 
 variable "zone" {
+  type    = string
   default = "us-central1-b"
 }
 
-variable "gke_master_ipv4_cidr_block" {
-default        = "10.2.0.0/21"
+variable "gke_master_ipv4_cidr_block"{
+ 
+  variable "mysql_location_preference" {
+  type = string
+  default = "europe-west1-b"
+}
+
+variable "mysql_machine_type" {
+  type = string
+  default = "db-n1-standard-2"
+}
+
+variable "mysql_database_version" {
+  type = string
+  default = "MYSQL_8_0"
+}
+
+variable "mysql_default_disk_size" {
+  type = string
+  default = "100"
+}
+
+variable "mysql_availability_type" {
+  type = string
+  default = "REGIONAL"
+}default        = "10.2.0.0/21"
   }
 
 variable "mysql_location_preference" {
